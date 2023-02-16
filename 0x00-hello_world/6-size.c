0be18e619bbdcf39b4fe93c13c06b0f8f2259714
SlaveToPurpose <keziahleroux@gmail.com>
2023-02-16 13:30:59 -0800

    hot fix task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,9 +15,9 @@ int main(void)
         long long int llnum;
         float dec;
 
-        printf("Size of a char: %zu byte(s)\n", sizeof(ch));
-        printf("Size of an int: %zu byte(s)\n", sizeof(num));
-        printf("Size of a long int: %zu byte(s)\n", sizeof(lnum));
-        printf("Size of a long long int: %zu byte(s)\n", sizeof(llnum));
-        printf("Sizeof a float: %zu byte(s)\n", sizeof(dec));
+        printf("Size of a char: %i byte(s)\n", sizeof(ch));
+        printf("Size of an int: %i byte(s)\n", sizeof(num));
+        printf("Size of a long int: %i byte(s)\n", sizeof(lnum));
+        printf("Size of a long long int: %i byte(s)\n", sizeof(llnum));
+        printf("Sizeof a float: %i byte(s)\n", sizeof(dec));
         return (0);
