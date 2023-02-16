e7eb1b1467694b64787d3f268f2813407243b16f
SlaveToPurpose <keziahleroux@gmail.com>
2023-02-16 13:28:05 -0800

    print size of vars

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,15 +9,15 @@
 
 int main(void)
 {
-	char ch;
-	int num;
-	long int lnum;
-	long long int llnum;
-	float dec;
+        char ch;
+        int num;
+        long int lnum;
+        long long int llnum;
+        float dec;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(ch));
-	printf("Size of an int: %zu byte(s)\n", sizeof(num));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(lnum));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(llnum));
-	printf("Sizeof a float: %zu byte(s)\n", sizeof(dec));
-	return (0);
+        printf("Size of a char: %zu byte(s)\n", sizeof(ch));
+        printf("Size of an int: %zu byte(s)\n", sizeof(num));
+        printf("Size of a long int: %zu byte(s)\n", sizeof(lnum));
+        printf("Size of a long long int: %zu byte(s)\n", sizeof(llnum));
+        printf("Sizeof a float: %zu byte(s)\n", sizeof(dec));
+        return (0);
