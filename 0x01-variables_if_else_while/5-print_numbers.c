9b74e9ab0a03614535e9eabc3a1b3a7e541ed4db
SlaveToPurpose <keziahleroux@gmail.com>
2023-02-17 12:09:13 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print single digits of base10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+
+	printf("\n");
+
+	return (0);
+}
