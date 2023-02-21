8b69e3636a8822b615498ee812c4dd1c5d74f779
SlaveToPurpose <keziahleroux@gmail.com>
2023-02-21 07:25:57 -0800

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if char is an alphabet
+ * Return: 1 if alphabetci char 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
