642a6f4416506e81fabbb0447834c697ee9dafd2
SlaveToPurpose <keziahleroux@gmail.com>
2023-02-21 23:29:52 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @i: interger 1
+ * @j: integer 2
+ * Return: result of add
+ */
+
+int add(int i, int j)
+{
+	int g = i + j;
+
+	return (g);
+}
