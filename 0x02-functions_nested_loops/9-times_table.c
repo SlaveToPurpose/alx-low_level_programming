79f9c0141f6f04f695ce507c274312b4ebe188f5
SlaveToPurpose <keziahleroux@gmail.com>
2023-02-21 16:09:24 -0800

    joh task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,38 +6,31 @@
 
 void times_table(void)
 {
-	int num1;
+	int num1, num2, y;
 
 	for (num1 = 0; num1 <= 9; num1++)
-        {
-			_putchar((0 * num1) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar((1 * num1) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar((2 * num1) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar((3 * num1) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar((4 * num1) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar((5 * num1) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar((6 * num1) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar((7 * num1) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar((8 * num1) + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar((9 * num1) + '0');
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			y = num2 * num1;
+			if ( y > 9)
+			{
+				_putchar((y/10) + '0');
+				_putchar((y%10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(y + '0');
+			}
+
+			if (num2 < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+		}
 
 			_putchar('\n');
 	}
