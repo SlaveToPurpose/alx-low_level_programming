285d7cb40bc1645cf9013bb5d6e6897228fa9f69
SlaveToPurpose <keziahleroux@gmail.com>
2023-02-23 14:10:14 -0800

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - print a square with #
+ * @size: integer for size
+ */
+
+void print_square(int size)
+{
+	int j, k;
+
+	for (k = 0; k < size; k++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
