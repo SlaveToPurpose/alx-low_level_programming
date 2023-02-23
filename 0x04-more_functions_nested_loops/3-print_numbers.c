ba25eeb660153231c783e173d830334c92553ce1
SlaveToPurpose <keziahleroux@gmail.com>
2023-02-23 07:02:31 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print digits 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int j;
+
+	for (j = 48; j <= 57; j++)
+	{
+		_putchar(j);
+	}
+
+	_putchar('\n');
+}
