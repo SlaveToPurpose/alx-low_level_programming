4505b06e825f2878f0b1db870ad408b5f89b9d3e
SlaveToPurpose <keziahleroux@gmail.com>
2023-02-28 03:04:23 -0800

    TAask 5 again

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,19 +6,33 @@
  * @s: array of chars
  */
 
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 void rev_string(char *s)
 {
 	int g;
 	int a;
-	char jk;
+	char k;
 
 	for (a = 0; s[a] != '\0'; a++)
 	{}
 
 	for (g = a - 1; g >= 0; g--)
 	{
-		jk = s[g];
-		_putchar(jk);
+		k = s[g];
+		_putchar(k);
 	}
 
 	_putchar('\n');
