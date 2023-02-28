65ae8aa95613e5fea573f17d477a0c3ea3ef65e3
SlaveToPurpose <keziahleroux@gmail.com>
2023-02-28 05:11:56 -0800

    TAsk 7 attempt 1

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of string
+ *
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int k;
+	int j;
+	int g;
+
+	for (k = 0; str[k] != '\0'; k++)
+	{}
+
+	for (j = (k / 2); j <= k; j++)
+	{
+		g = str[j];
+		_putchar(g);
+	}
+
+	_putchar('\n');
+}
