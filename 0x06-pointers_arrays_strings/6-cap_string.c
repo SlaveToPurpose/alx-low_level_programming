d885c419d509b58516db836617f0a47861257e06
SlaveToPurpose <keziahleroux@gmail.com>
2023-03-02 10:28:05 -0800

    Task 6 attempt

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalise all words
+ *
+ * @strToCap: string parameter to capitalise
+ * Return: capitalised string
+ */
+
+
+char *cap_string(char *strToCap)
+{
+	int g;
+	int a;
+
+	/* find length of string */
+	for (g = 0; strToCap[g] != '\0'; g++)
+	{}
+
+	if (g == 0)
+		return (strToCap);
+
+	/* loop through string to test for word seperators */
+	for (a = 1; a < g; a++)
+	{
+		if (strToCap[a] == ' '
+				|| strToCap[a] == '\t'
+				|| strToCap[a] == '\n'
+				|| strToCap[a] == ','
+				|| strToCap[a] == ';'
+				|| strToCap[a] == '.'
+				|| strToCap[a] == '!'
+				|| strToCap[a] == '?'
+				|| strToCap[a] == 34
+				|| strToCap[a] == '('
+				|| strToCap[a] == ')'
+				|| strToCap[a] == '{'
+				|| strToCap[a] == '}')
+		{
+			if (strToCap[a + 1] != '\0'
+					&& strToCap[a + 1] < 123
+					&& strToCap[a + 1] > 96)
+			{
+				strToCap[a + 1] = strToCap[a + 1] - 32;
+			}
+		}
+	}
+
+	return (strToCap);
+}
