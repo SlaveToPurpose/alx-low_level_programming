fa4727fbcabeb769e38b6d1f146a8fe62b9a08a4
SlaveToPurpose <keziahleroux@gmail.com>
2023-03-02 13:28:09 -0800

    task 101 attempt 2

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,11 +14,17 @@ void print_number(int n)
 		n = -n;
 	}
 
-	if (n / 10)
+	if (n < 10)
 	{
-		print_number(n / 10);
+		_putchar(n + 48);
 	}
+	else
+	{
+		if (n / 10)
+		{
+			print_number(n / 10);
+		}
 
-	_putchar(n % 10 + 48);
-
+		_putchar(n % 10 + 48);
+	}
 }
