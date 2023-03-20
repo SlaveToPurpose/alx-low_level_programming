792f5a1b28dcdd5f1e9fef8568847d802283dd93
SlaveToPurpose <keziahleroux@gmail.com>
2023-03-20 12:41:31 -0700

    task 0, create struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - contains info for poppy the dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
