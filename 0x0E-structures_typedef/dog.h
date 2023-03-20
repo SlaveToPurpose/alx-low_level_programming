0d8f16b7ec04880becc75c2a8b6a094825425122
SlaveToPurpose <keziahleroux@gmail.com>
2023-03-20 13:47:44 -0700

    task 3, define new type dog_t for struct dog to dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,10 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+ * dog_t - typedef for new struct dog_t
+ */
+
+typedef struct dog dog_t;
+
 #endif /*DOG_H*/
