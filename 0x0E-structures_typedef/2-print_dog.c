39370868e26c1426eacb9c17d88e3fa2eff872a3
SlaveToPurpose <keziahleroux@gmail.com>
2023-03-20 13:33:20 -0700

    Task 2, print struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints whole struct dog
+ *
+ * @d: poniter to struct to be printed
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		d->name = "(nil)";
+
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	if (d == NULL)
+		return;
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
