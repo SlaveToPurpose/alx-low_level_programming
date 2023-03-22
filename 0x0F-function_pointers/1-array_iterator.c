e5342b2316fa95140bbc5d27e61534b4031f3da3
SlaveToPurpose <keziahleroux@gmail.com>
2023-03-22 04:33:15 -0700

    Execute function on each element of array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - execute function given as param to each element
+ *
+ * @array: array to iterate through
+ * @size: size of array
+ * @action: pointer to function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int glory;
+
+	/*check for NULLS*/
+	if (array == NULL || action == NULL || size == 0)
+		return;
+
+	/*iterate through array*/
+	glory = 0;
+	while (glory < size)
+	{
+		action(array[glory]);
+		glory++;
+	}
+
+}
