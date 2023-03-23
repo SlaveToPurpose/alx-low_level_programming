a004f443f577f652969069d683382b2b580b28a1
SlaveToPurpose <keziahleroux@gmail.com>
2023-03-23 13:12:18 -0700

    string to be printed, with given separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * print_numbers - prints numbers
+ * @separator: string to be printed between numbers
+ * @n: number of arguments
+ * @...: A variable number of arguments
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list arguments;
+	unsigned int itterator;
+
+	va_start(arguments, n);
+	for (itterator = 0; itterator < n; itterator++)
+	{
+		printf("%d", va_arg(arguments, int));
+		if (itterator != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(arguments);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_strings - prints strings
+ * @separator: string to be printed between strings
+ * @n: number of strings passed to function
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	unsigned int itterator;
+	char *strVar;
+
+	va_start(strings, n);
+
+	for (itterator = 0; itterator < n; itterator++)
+	{
+		strVar = va_arg(strings, char *);
+
+		if (strVar == NULL)
+			printf("(nil)");
+		else
+			printf("%s", strVar);
+
+		if ((itterator != (n - 1)) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(strings);
+}
