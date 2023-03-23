b13ce7d8c5ffc97865e59af15e9a45f29f894487
SlaveToPurpose <keziahleroux@gmail.com>
2023-03-23 08:10:18 -0700

    variadic func to sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all parameters
+ * @n: number of digits to sum
+ * Return: 0 upon failure, sum of parameters on success
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		va_list arguments;
+		int summation;
+		unsigned int itterator;
+		int sumNum;
+
+		va_start(arguments, n);
+		summation = 0;
+
+		for (itterator = 0; itterator < n; itterator++)
+		{
+			sumNum = va_arg(arguments, int);
+			summation += sumNum;
+		}
+
+		va_end(arguments);
+		return (summation);
+	}
+}
