f5c3281146f2335423d6c22ac796c85ab9f206c7
SlaveToPurpose <keziahleroux@gmail.com>
2023-04-04 09:20:38 -0700

    pop head off list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes head node
+ * @head: double pointer to head of list
+ * Return: data that was popped off list on sucess, else 0
+ */
+
+int pop_listint(listint_t **head)
+{
+	/*listint_t *newHead;*/
+	listint_t *currentHead;
+	int popData;
+
+	currentHead = *head;
+
+	if (currentHead == NULL)
+	{
+		return (0);
+	}
+
+	/*newHead = currentHead->next;*/
+	popData = currentHead->n;
+	free(currentHead);
+
+	*head = NULL;
+
+	return (popData);
+}
