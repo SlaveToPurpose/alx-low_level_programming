844f44a6f21c33ecba49caa64726bac3d0f7a44d
SlaveToPurpose <keziahleroux@gmail.com>
2023-04-04 02:44:03 -0700

    delete linked list and set head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a list and sets head to null
+ * @head: double pointer to head of list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+	listint_t *tempNode;
+
+	current = *head;
+
+	while (current != NULL)
+	{
+		tempNode = current->next;
+		free(current);
+		current = tempNode;
+	}
+
+	*head = NULL;
+}
