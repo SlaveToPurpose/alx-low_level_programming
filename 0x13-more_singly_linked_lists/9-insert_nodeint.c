2bc6348fd82584355bf00b65aca79d5726d953c1
SlaveToPurpose <keziahleroux@gmail.com>
2023-04-06 12:38:45 -0700

    fixed index zero issue

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -42,8 +42,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		newNode->next = currNode;
-		currNode = newNode;
+		newNode->next = *head;
+		*head = newNode;
 		return (newNode);
 	}
 
