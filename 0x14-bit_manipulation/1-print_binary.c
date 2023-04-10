0a91b7b6254f90e422edb3d23c7d0650d7ba8d9c
SlaveToPurpose <keziahleroux@gmail.com>
2023-04-10 05:52:56 -0700

    adjusted algorithm to use recursion to convert

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,16 +7,15 @@
 
 void print_binary(unsigned long int n)
 {
-	int i;
-	unsigned long int k;
+		if (n >> 0)
+		{
+			if (n >> 1)
+			{
+				print_binary(n >> 1);
+			}
 
-	for (i = 31; i >= 0; i--)
-	{
-		k = n >> i;
-
-		if (k & 1)
-			_putchar('1');
+			_putchar((n & 1) + 48);
+		}
 		else
 			_putchar('0');
-	}
 }
